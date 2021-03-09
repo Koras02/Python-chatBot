ead890f3950e1f5653b5ded0fb0d6d2a6a112325
Koras02 <wjdgh0727@gmail.com>
2021-03-10 00:02:57 +0900

    파이썬으로 디스코드 봇 만들기 #1 - 메시지에 답하는 기능 추가

--- Chat.py
+++ Chat.py
@@ -16,7 +16,23 @@ async def on_ready():
     game = discord.Game('봇이 활동중에 표시 될 이름')
     await client.change_presence(status=discord.Status.online, activity=game)
 
-client.run(token)
 #  기본작성 끝
 
-# pip3 -install
+# 메세지 답하는 기능 추가
+
+
+@client.event
+async def on_message(message):
+    if message.content == "그렇구만":
+        await message.channel.send("하하하하!")
+
+    if message.content == "누구냐고":
+        await message.channel.send("누구긴 누구야!! 너지!")
+
+    if message.content == "내일 점심 뭐먹을까??":
+        await message.channel.send("소금듬뿍 들어간 돼지고기 김치주먹밥을 추천할께 친구야!!")
+
+client.run(token)
+
+
+# pip3 -install pip3 설치해준다.
